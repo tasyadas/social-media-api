c3121394c5c41472e8462d2cb70453d010299410
tasyadas <tasyadwiaskarasiahaan@gmail.com>
2021-08-10 19:20:49 +0700

    add get_all_tag_with_relation method

--- models/tag.rb
+++ models/tag.rb
@@ -1,4 +1,5 @@
 require_relative '../db/mysql_connector'
+require_relative './tweet'
 
 class Tag
 
@@ -22,5 +23,39 @@ class Tag
     )
 
     true
+  end
+
+  def self.get_all_tag_with_relation
+    db_raw = create_db_client.query(
+      'SELECT tags.*, ' +
+      'tag_tweet.tweet_id AS tweet_id, ' +
+      'tag_comment.comment_id AS comment_id ' +
+      'FROM tags ' +
+      'LEFT JOIN tag_tweet ON tag_tweet.tag_id = tag.id ' +
+      'LEFT JOIN tag_comment ON tag_comment.tag_id = tag.id'
+    )
+
+    tags = Array.new
+
+    db_raw.each do |data|
+
+      tweet   = data['tweet_id'] ? Tweet.find_single_tweet(data['tweet_id']) : null
+      comment = data['comment_id'] ? Comment.find_single_comment(data['comment_id']) : null
+      tag     = tags.find{|h| h.id == data['id']}
+
+      if tag
+        tag.tweets.push(tweet) if tweet
+        tag.comments.push(comment) if comment
+      else
+        tag = Tag.new({
+          :id     => data["id"],
+          :name   => data["name"],
+        })
+
+        tags.push(tag)
+      end
+    end
+
+    tags
   end
 end

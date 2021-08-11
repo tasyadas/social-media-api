001f23e2af2d3a6091a012fab7284b53fdfd7327
tasyadas <tasyadwiaskarasiahaan@gmail.com>
2021-08-11 12:16:01 +0700

    add get_last_item method in tag model and it's test case

--- models/tag.rb
+++ models/tag.rb
@@ -3,14 +3,16 @@ require_relative './tweet'
 
 class Tag
 
-  attr_accessor :id, :name, :tweets, :comments
+  attr_accessor :id, :name, :tweets, :comments, :created_at, :updated_at
 
-  def initialize(param)
-    @id        = param.key?(:id) ? param[:id] : nil
-    @name      = param[:name]
-    @tweets    = param.key?(:tweets) ? param[:tweets] : []
-    @comments  = param.key?(:comments) ? param[:comments] : []
-  end
+    def initialize(param)
+      @id         = param.key?(:id) ? param[:id] : nil
+      @name       = param[:name]
+      @tweets     = param.key?(:tweets) ? param[:tweets] : []
+      @comments   = param.key?(:comments) ? param[:comments] : []
+      @created_at = param.key?(:created_at) ? param[:created_at] : nil
+      @updated_at = param.key?(:updated_at) ? param[:updated_at] : nil
+    end
 
   def save
     create_db_client.query(
@@ -31,16 +33,16 @@ class Tag
       'tag_tweet.tweet_id AS tweet_id, ' +
       'tag_comment.comment_id AS comment_id ' +
       'FROM tags ' +
-      'LEFT JOIN tag_tweet ON tag_tweet.tag_id = tag.id ' +
-      'LEFT JOIN tag_comment ON tag_comment.tag_id = tag.id'
+      'LEFT JOIN tag_tweet ON tag_tweet.tag_id = tags.id ' +
+      'LEFT JOIN tag_comment ON tag_comment.tag_id = tags.id'
     )
 
     tags = Array.new
 
     db_raw.each do |data|
 
-      tweet   = data['tweet_id'] ? Tweet.find_single_tweet(data['tweet_id']) : null
-      comment = data['comment_id'] ? Comment.find_single_comment(data['comment_id']) : null
+      tweet   = data['tweet_id'] ? Tweet.find_single_tweet(data['tweet_id']) : nil
+      comment = data['comment_id'] ? Comment.find_single_comment(data['comment_id']) : nil
       tag     = tags.find{|h| h.id == data['id']}
 
       if tag
@@ -48,8 +50,10 @@ class Tag
         tag.comments.push(comment) if comment
       else
         tag = Tag.new({
-          :id     => data["id"],
-          :name   => data["name"],
+          :id         => data["id"],
+          :name       => data["name"],
+          :created_at => data['created_at'],
+          :updated_at => data['updated_at']
         })
 
         tags.push(tag)
@@ -64,6 +68,16 @@ class Tag
 
     if tag.nil?
       raise "Tag with id #{id} not found"
+    end
+
+    tag
+  end
+
+  def self.get_last_item
+    tag = self.get_all_tag_with_relation.max_by{|x| x.created_at}
+
+    if tag.nil?
+      raise "There is no Tag"
     end
 
     tag

5d12bd56fa2fe0c4d08e7f977a0dfb7f2176989c
Tasya Dwi A. S <tasyadwiaskarasiahaan@gmail.com>
2021-09-03 15:18:11 +0700

    fixing bugs

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../models/tag'
+require 'date'
 
 class TagController
 
@@ -9,7 +10,7 @@ class TagController
   end
 
   def self.get_five_trending
-    tags = Tag.get_all_tag_with_relation.select { |tag| tag.created_at - (24/24.0)}
+    tags = Tag.get_all_tag_with_relation.select { |tag| DateTime.parse(tag.created_at.to_s) >= (DateTime.now - (24/24.0))}
     return 'There is no trending hashtag found' if tags.length == 0
     tags.sort_by! {|tag| -(tag.tweets.length + tag.comments.length)}.take(5)
   end
